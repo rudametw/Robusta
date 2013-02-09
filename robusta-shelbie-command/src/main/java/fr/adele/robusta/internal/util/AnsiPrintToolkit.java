4338d4ee2677fbcc5d9d22a10fbc15b8640dcd6d
rudametw <rudametkin@gmail.com>
2013-02-09 16:29:43 +0100

    Added tree lines when printing tree.

--- robusta-shelbie-command/src/main/java/fr/adele/robusta/internal/util/AnsiPrintToolkit.java
+++ robusta-shelbie-command/src/main/java/fr/adele/robusta/internal/util/AnsiPrintToolkit.java
@@ -18,215 +18,227 @@ package fr.adele.robusta.internal.util;
 import org.fusesource.jansi.Ansi;
 
 /**
- * Simple toolkit to ease manipulation of the ANSI buffer. TODO, maybe this can be refactored as a service (API +
- * Implementation) ...
+ * Simple toolkit to ease manipulation of the ANSI buffer. TODO, maybe this can
+ * be refactored as a service (API + Implementation) ...
  */
 public class AnsiPrintToolkit {
 
-	/**
-	 * Default indentation.
-	 */
-	private static final String DEFAULT_INDENTER = "  ";
+    /**
+     * Default indentation.
+     */
+    private static final String DEFAULT_INDENTER = "  ";
 
-	/**
-	 * Ansi buffer.
-	 */
-	private Ansi buffer;
+    /**
+     * Ansi buffer.
+     */
+    private Ansi buffer;
 
-	/**
-	 * Verbosity mode.
-	 */
-	private boolean verbose = false;
+    /**
+     * Verbosity mode.
+     */
+    private boolean verbose = false;
 
-	/**
-	 * Indentation value.
-	 */
-	private String indenter = DEFAULT_INDENTER;
+    /**
+     * Indentation value.
+     */
+    private String indenter = DEFAULT_INDENTER;
 
-	public AnsiPrintToolkit() {
-		this(Ansi.ansi());
-	}
+    public AnsiPrintToolkit() {
+        this(Ansi.ansi());
+    }
 
-	public AnsiPrintToolkit(final Ansi ansi) {
-		buffer = ansi;
-	}
+    public AnsiPrintToolkit(final Ansi ansi) {
+        buffer = ansi;
+    }
 
-	public boolean isVerbose() {
-		return verbose;
-	}
+    public boolean isVerbose() {
+        return verbose;
+    }
 
-	public void setVerbose(final boolean verbose) {
-		this.verbose = verbose;
-	}
+    public void setVerbose(final boolean verbose) {
+        this.verbose = verbose;
+    }
 
-	public Ansi getBuffer() {
-		return buffer;
-	}
+    public Ansi getBuffer() {
+        return buffer;
+    }
 
-	public String getIndenter() {
-		return indenter;
-	}
+    public String getIndenter() {
+        return indenter;
+    }
 
-	public void setIndenter(final String indenter) {
-		this.indenter = indenter;
-	}
+    public void setIndenter(final String indenter) {
+        this.indenter = indenter;
+    }
 
-	public static boolean isEmpty(final String value) {
-		return ((value == null) || ("".equals(value)));
-	}
+    public static boolean isEmpty(final String value) {
+        return ((value == null) || ("".equals(value)));
+    }
 
-	public void indent() {
-		indent(1);
-	}
+    public void indent() {
+        indent(1);
+    }
 
-	public void indent(final int level) {
-		for (int i = 0; i < level; i++) {
-			buffer.a(indenter);
-		}
-	}
+    public void indent(final int level) {
+        for (int i = 0; i < level; i++) {
+            buffer.a(indenter);
+        }
+    }
 
-	public void eol() {
-		eol(1);
-	}
+    public void indentWithMarker(final int level, final int length) {
+        if (level <= 0) return;
+        for (int i = 0; i < level - 1; i++) {
+            indent(length);
+            white("│");
+        }
+        // indent(1);
+        indent(length);
+        white("├");
+        white("───");
+    }
 
-	public void eol(final int level) {
-		for (int i = 0; i < level; i++) {
-			buffer.a('\n');
-		}
-	}
+    public void eol() {
+        eol(1);
+    }
 
-	public void red(final String message) {
-		color(message, Ansi.Color.RED);
-	}
+    public void eol(final int level) {
+        for (int i = 0; i < level; i++) {
+            buffer.a('\n');
+        }
+    }
 
-	public void green(final String message) {
-		color(message, Ansi.Color.GREEN);
-	}
+    public void red(final String message) {
+        color(message, Ansi.Color.RED);
+    }
 
-	public void blue(final String message) {
-		color(message, Ansi.Color.BLUE);
-	}
+    public void green(final String message) {
+        color(message, Ansi.Color.GREEN);
+    }
 
-	public void white(final String message) {
-		color(message, Ansi.Color.WHITE);
-	}
+    public void blue(final String message) {
+        color(message, Ansi.Color.BLUE);
+    }
 
-	public void black(final String message) {
-		color(message, Ansi.Color.BLACK);
-	}
+    public void white(final String message) {
+        color(message, Ansi.Color.WHITE);
+    }
 
-	public void cyan(final String message) {
-		color(message, Ansi.Color.CYAN);
-	}
+    public void black(final String message) {
+        color(message, Ansi.Color.BLACK);
+    }
 
-	public void yellow(final String message) {
-		color(message, Ansi.Color.YELLOW);
-	}
+    public void cyan(final String message) {
+        color(message, Ansi.Color.CYAN);
+    }
 
-	public void magenta(final String message) {
-		color(message, Ansi.Color.MAGENTA);
-	}
+    public void yellow(final String message) {
+        color(message, Ansi.Color.YELLOW);
+    }
 
-	public void color(final String message, final Ansi.Color color) {
-		buffer.fg(color);
-		buffer.a(message);
-		buffer.fg(Ansi.Color.DEFAULT);
-	}
+    public void magenta(final String message) {
+        color(message, Ansi.Color.MAGENTA);
+    }
 
-	public void italic(final String message) {
-		buffer.a(Ansi.Attribute.ITALIC);
-		buffer.a(message);
-		buffer.a(Ansi.Attribute.ITALIC_OFF);
-	}
+    public void color(final String message, final Ansi.Color color) {
+        buffer.fg(color);
+        buffer.a(message);
+        buffer.fg(Ansi.Color.DEFAULT);
+    }
 
-	public void bold(final String message) {
-		buffer.a(Ansi.Attribute.INTENSITY_BOLD);
-		buffer.a(message);
-		buffer.a(Ansi.Attribute.INTENSITY_BOLD_OFF);
-	}
+    public void italic(final String message) {
+        buffer.a(Ansi.Attribute.ITALIC);
+        buffer.a(message);
+        buffer.a(Ansi.Attribute.ITALIC_OFF);
+    }
 
-	public void underline(final String message) {
-		buffer.a(Ansi.Attribute.UNDERLINE);
-		buffer.a(message);
-		buffer.a(Ansi.Attribute.UNDERLINE_OFF);
-	}
+    public void bold(final String message) {
+        buffer.a(Ansi.Attribute.INTENSITY_BOLD);
+        buffer.a(message);
+        buffer.a(Ansi.Attribute.INTENSITY_BOLD_OFF);
+    }
 
-	public void print(final String message) {
-		buffer.a(message);
-	}
+    public void underline(final String message) {
+        buffer.a(Ansi.Attribute.UNDERLINE);
+        buffer.a(message);
+        buffer.a(Ansi.Attribute.UNDERLINE_OFF);
+    }
 
-	public void separator() {
-		buffer.fg(Ansi.Color.YELLOW);
-		bold(":");
-		buffer.fg(Ansi.Color.DEFAULT);
-	}
+    public void print(final String message) {
+        buffer.a(message);
+    }
 
-	public void pad(final String message, final int repetitions) {
-		for (int i = 0; i < repetitions; i++) {
-			bold("*");
-		}
-	}
+    public void separator() {
+        buffer.fg(Ansi.Color.YELLOW);
+        bold(":");
+        buffer.fg(Ansi.Color.DEFAULT);
+    }
 
-	public void title(final String message) {
-		final String padding = "*****";
-		final int INDENT_SIZE = 4;
-		final int length = (2 * padding.length()) + message.length() + (2 * INDENT_SIZE * DEFAULT_INDENTER.length());
+    public void pad(final String message, final int repetitions) {
+        for (int i = 0; i < repetitions; i++) {
+            bold("*");
+        }
+    }
 
-		buffer.fg(Ansi.Color.BLUE);
-		eol();
-		pad("*", length);
-		eol();
-		bold(padding);
-		indent(INDENT_SIZE);
-		bold(message);
-		indent(INDENT_SIZE);
-		bold(padding);
-		eol();
-		pad("*", length);
-		eol();
-		buffer.fg(Ansi.Color.DEFAULT);
-	}
+    public void title(final String message) {
+        final String padding = "*****";
+        final int INDENT_SIZE = 4;
+        final int length = (2 * padding.length()) + message.length() + (2 * INDENT_SIZE * DEFAULT_INDENTER.length());
 
-	public void subtitle(final String message) {
-		final String padding = "***";
-		final int INDENT_SIZE = 1;
+        buffer.fg(Ansi.Color.BLUE);
+        eol();
+        pad("*", length);
+        eol();
+        bold(padding);
+        indent(INDENT_SIZE);
+        bold(message);
+        indent(INDENT_SIZE);
+        bold(padding);
+        eol();
+        pad("*", length);
+        eol();
+        buffer.fg(Ansi.Color.DEFAULT);
+    }
 
-		buffer.fg(Ansi.Color.BLUE);
-		bold(padding);
-		indent(INDENT_SIZE);
-		bold(message);
-		indent(INDENT_SIZE);
-		bold(padding);
-		eol();
-		buffer.fg(Ansi.Color.DEFAULT);
-	}
+    public void subtitle(final String message) {
+        final String padding = "***";
+        final int INDENT_SIZE = 1;
 
-	public void urgent(final String message) {
-		buffer.bgBright(Ansi.Color.RED);
-		buffer.fg(Ansi.Color.WHITE);
-		bold(message);
-		buffer.fg(Ansi.Color.DEFAULT);
-		buffer.bg(Ansi.Color.DEFAULT);
-		eol();
-	}
+        buffer.fg(Ansi.Color.BLUE);
+        bold(padding);
+        indent(INDENT_SIZE);
+        bold(message);
+        indent(INDENT_SIZE);
+        bold(padding);
+        eol();
+        buffer.fg(Ansi.Color.DEFAULT);
+    }
 
-	public void bold(final int message) {
-		buffer.a(Ansi.Attribute.INTENSITY_BOLD);
-		buffer.a(message);
-		buffer.a(Ansi.Attribute.INTENSITY_BOLD_OFF);
-	}
+    public void urgent(final String message) {
+        buffer.bgBright(Ansi.Color.RED);
+        buffer.fg(Ansi.Color.WHITE);
+        bold(message);
+        buffer.fg(Ansi.Color.DEFAULT);
+        buffer.bg(Ansi.Color.DEFAULT);
+        eol();
+    }
 
-	public static String padRight(final String s, final int n) {
-		return String.format("%1$-" + n + "s", s);
-	}
+    public void bold(final int message) {
+        buffer.a(Ansi.Attribute.INTENSITY_BOLD);
+        buffer.a(message);
+        buffer.a(Ansi.Attribute.INTENSITY_BOLD_OFF);
+    }
 
-	public static String padLeft(final String s, final int n) {
-		return String.format("%1$" + n + "s", s);
-	}
+    public static String padRight(final String s, final int n) {
+        return String.format("%1$-" + n + "s", s);
+    }
 
-	public void debug(final String string) {
-		yellow("DEBUG: " + string);
-		eol();
-	}
+    public static String padLeft(final String s, final int n) {
+        return String.format("%1$" + n + "s", s);
+    }
+
+    public void debug(final String string) {
+        yellow("DEBUG: " + string);
+        eol();
+    }
 
 }
