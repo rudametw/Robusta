0f44f84d05d199c6ad6c27bfa13a2f46bc1c4719
rudametw <rudametkin@gmail.com>
2013-02-10 18:03:23 +0100

    Changed EmptyClass to see what information gets compiled into bytecode and what doesn't.

--- robusta-shelbie-command/src/main/java/fr/adele/robusta/EmptyClass.java
+++ robusta-shelbie-command/src/main/java/fr/adele/robusta/EmptyClass.java
@@ -1,5 +1,6 @@
 package fr.adele.robusta;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -9,4 +10,12 @@ public class EmptyClass {
 
 	List<Class<?>> mylist = null;
 	Set<ClassDescription> descriptions;
+
+	public void method(){
+
+	    //TRICKY ONE HERE... generic not kept in Bytecode
+	    new ArrayList<fr.adele.robusta.ClassAction>();
+
+	    return;
+	}
 }
